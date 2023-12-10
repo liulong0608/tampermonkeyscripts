db7c9a8538c5af9272ed7190c93a29a673bd347f
echo <tzu-mingliu@qq.com>
2023-12-10 21:05:51 +0800

    v0.1.0: feat: 新建腾讯问卷脚本

---
+++ 腾讯问卷.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         腾讯问卷
+// @namespace    https://gitlab.com/liulong-Echo/tampermonkeyscripts.git
+// @version      0.1
+// @description  油猴刷腾讯问卷（开发版）
+// @author       echo
+// @match        https://wj.qq.com/*
+// @icon         none
+// @grant        none
+// ==/UserScript==
+(function () {
+    'use strict';
+    console.log("腾讯问卷");
+
+})();
